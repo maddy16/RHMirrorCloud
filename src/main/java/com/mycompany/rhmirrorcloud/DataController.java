b61b151b21630240f51bee79544ea4e1f73f8ae6
ahmed <ahmed@192.168.10.5>
2016-12-30 03:39:06 +0500

    Changed File  Separator in 2 locations

--- src/main/java/com/mycompany/rhmirrorcloud/DataController.java
+++ src/main/java/com/mycompany/rhmirrorcloud/DataController.java
@@ -53,7 +53,7 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Controller
 public class DataController {
-    public static String LOCATION="mirrorWebUploads"+File.pathSeparator;
+    public static String LOCATION="mirrorWebUploads"+File.separator;
     @Autowired
     PushedFileDao fileDao;
     @Autowired
